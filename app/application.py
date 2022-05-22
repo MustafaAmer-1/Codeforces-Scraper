dd6b6cb5008fbe53465a2cd2af1567c4600993a1
Mustafa Amer <1806185@eng.asu.edu.eg>
2022-05-22 21:17:15 +0200

    update application module

--- app/application.py
+++ app/application.py
@@ -1,31 +1,37 @@
-import requests
+import requests, os
 from time import sleep
-from healper import process_data, dumb_visited_problems
+from healper import process_data, dumb_visited_problems, login
+from cookies import cookieHelper
 URL = "https://codeforces.com/api/user.status"
-HANDLE = "MustafaAmer-1"
 FROM = 1
 COUNT = 20
 
 def app():
-    params = {
-        'handle' : HANDLE,
-        'from' : FROM,
-        'count' : COUNT
-    }
-    res = requests.get(URL, params=params)
-    print('...Processing...')
-    while(res.ok):
-        data = res.json()
-        process_data(data)
-        dumb_visited_problems()
-        if(data['result']):
-            params['from'] += COUNT
-            sleep(2.5)
-            res = requests.get(URL, params=params)
-        else:
-            print('...Done...')
-            return
-    print('Request Problem...\nstatus_code: ' + res.status_code)
+    login()
+    cookies = cookieHelper()
+    if cookies == None:
+        print("No cookies found!!")
+        return
+    with requests.Session() as s:
+        params = {
+            'handle' : os.environ['CF_USER'],
+            'from' : FROM,
+            'count' : COUNT
+        }
+        res = s.get(URL, params=params)
+        print('...Processing...')
+        while(res.ok):
+            data = res.json()
+            process_data(data)
+            dumb_visited_problems()
+            if(data['result']):
+                params['from'] += COUNT
+                sleep(2.5)
+                res = requests.get(URL, params=params)
+            else:
+                print('...Done...')
+                return
+        print('Request Problem...\nstatus_code: ' + res.status_code)
 
 if __name__ == '__main__':
     app()
